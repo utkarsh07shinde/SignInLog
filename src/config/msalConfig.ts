285d829b74057eb875e84afbdbc72ed3b1b834e7
utkarsh07shinde <utkarsh07shinde@gmail.com>
2026-02-19 10:36:29 +0530

    Authentication Added

--- src/config/msalConfig.ts
+++ src/config/msalConfig.ts
@@ -3,14 +3,10 @@
 
  export const msalConfig = {
  auth: {
-  clientId: 'd12df531-8e2c-4d85-ac14-ab56707004c6',
-  authority: 'https://login.microsoftonline.com/1e499b00-b34d-4461-b219-a45d602ca97c',
-  redirectUri: 'https://crowdstrike-style-1.vercel.app/', // Replace with your actual redirect URI
-  postLogoutUri: 'https://crowdstrike-style-1.vercel.app/'
-//  clientId: import.meta.env.VITE_AZURE_CLIENT_ID,
-  //  authority: `https://login.microsoftonline.com/${import.meta.env.VITE_AZURE_TENANT_ID}`,
-    //redirectUri: import.meta.env.VITE_REDIRECT_URI,
-    // postLogoutRedirectUri: import.meta.env.VITE_REDIRECT_URI
+ clientId: 'd12df531-8e2c-4d85-ac14-ab56707004c6',
+ authority: 'https://login.microsoftonline.com/1e499b00-b34d-4461-b219-a45d602ca97c',
+ redirectUri: 'https://sign-in-log.vercel.app/', // Replace with your actual redirect URI
+ postLogoutUri: 'https://sign-in-log.vercel.app/'
  },
  cache: {
  cacheLocation: 'sessionStorage', // This configures where your cache will be stored
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import fs from 'fs'
 
 export default defineConfig({
   plugins: [vue()],
@@ -8,5 +9,12 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.pem')),
+    },
+    port: 5173
   }
 })
